febfdb805b34d1c985c6805a0dade43ad4b8a883
Nayeem Arefin <137292799+Nayeem-Arefin@users.noreply.github.com>
2023-09-12 15:36:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//text-change code
+function firstText () {
+document.getElementById ('h2').innerHTML="Hello World";
+ }
+ function secondText () {
+document.getElementById ('h3').innerHTML="HTML mouse-event onclick is to be used to call the function by name within the HTML document.";
+ }
+ function thirdText (){
+document.getElementById ('h4').innerHTML="This output has been generated using the innerhtml function.";
+ }
+ function fourthText (){
+document.getElementById ('h5').innerhtml="Brendan Aich";
+
